65371afd5a2db27251199d2393f9e539934e78f7
Diego Felipe <diegofelipe.14@gmail.com>
2019-07-11 22:18:19 -0400

    Installing play sound lib

---
+++ app/src/components/PlaySound.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Sound from 'react-native-sound'
+
+
+export default PlaySound = _ => {
+
+    // Sound.setCategory('Playback')
+
+    // const sound = new Sound('https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3', null, (error) => {
+    //     if(error) {
+    //         console.log('failed')
+    //     } else {
+    //         console.log('worked')
+    //     }
+    //     sound.play()
+    // })
+    asdsad
+   
+
+
+}
